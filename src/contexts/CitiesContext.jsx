52fff5a85915c3b4a9790f934176cfa670300972
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2024-03-13 14:42:50 +0400

    Interacting with the map container: used custom hook(useGeolocation)

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -39,7 +39,14 @@ function CitiesProvider({ children }) {
   }
 
   return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+      }}
+    >
       {children}
     </CitiesContext.Provider>
   );
